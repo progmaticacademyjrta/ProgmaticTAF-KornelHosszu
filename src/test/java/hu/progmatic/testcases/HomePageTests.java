9bf564e512f4e1d13ded225dea14e15fc16e6844
95Kornel <115731433+95Kornel@users.noreply.github.com>
2023-02-04 18:09:40 +0100

    Update HomePageTests.java

--- src/test/java/hu/progmatic/testcases/HomePageTests.java
+++ src/test/java/hu/progmatic/testcases/HomePageTests.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 public class HomePageTests extends DriverBaseTest {
     HomePage homePage;
-    @Test(description = "TC01: Homepage loaded test that loads the page and validates whether the header is displayed or not.")
+    @Test(groups ="Smoke Test",description = "TC01: Homepage loaded test that loads the page and validates whether the header is displayed or not.")
     public void homepageLoadedTest() {
         homePage = new HomePage(driver, wait);
         homePage.loadHomePage();
